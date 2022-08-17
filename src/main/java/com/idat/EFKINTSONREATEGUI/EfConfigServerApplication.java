53d3039cda4fbc3082ae047cd7ee864f101803f3
KitsonR <reateguikitson98@gmail.com>
2022-08-17 01:23:08 -0500

    ConfigServer

---
+++ src/main/java/com/idat/EFKINTSONREATEGUI/EfConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EFKINTSONREATEGUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfConfigServerApplication.class, args);
+	}
+
+}
